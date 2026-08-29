fb0df5841fed83ae5bdd3b1df2368ec8c173a711
Umesh <sawantumesh395@gmail.com>
2026-08-29 18:24:28 +0530

    Apply @Validated on ReservationController

--- src/main/java/org/techhub/controller/ReservationController.java
+++ src/main/java/org/techhub/controller/ReservationController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import org.techhub.service.ReservationService;
 
 @RestController
 @RequestMapping("/reservations")
+@Validated
 public class ReservationController {
 
     private final ReservationService reservationService;
